6e33cc81764e94ae9e2ef66dec2e2723be35e001
Manojkumar <182618048+manojkumar-ctrl@users.noreply.github.com>
2026-08-15 18:57:50 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,29 @@
+class MinStack {
+ int min=Integer.MAX_VALUE;
+Stack<Integer> st;
+    public MinStack() {
+        st=new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(val<=min){
+            st.push(min);
+            min=val;
+        }
+        st.push(val);
+    }
+    
+    public void pop() {
+        if(st.pop()==min){
+            min=st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
