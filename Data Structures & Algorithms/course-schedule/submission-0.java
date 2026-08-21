17750c6946a0bcf5ba6877bc6e7476109d7505bb
Manojkumar <182618048+manojkumar-ctrl@users.noreply.github.com>
2026-08-21 15:08:37 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+          ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] p: prerequisites){
+            adj.get(p[1]).add(p[0]);
+        }
+
+        int[] indeg=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                indeg[it]++;
+            }
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.add(i);
+        }
+
+        ArrayList<Integer> res=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            res.add(node);
+            for(int it : adj.get(node)){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+
+        int[] ans=new int[res.size()];
+        for(int i=0;i<res.size();i++){
+            ans[i]=res.get(i);
+        }
+
+        if(numCourses==res.size()) return true;
+
+        return false;
+    }
+}
